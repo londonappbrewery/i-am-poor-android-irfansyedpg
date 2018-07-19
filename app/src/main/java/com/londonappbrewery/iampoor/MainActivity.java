2915c80a4f79d544451359aa02ce397b1e4a1591
syed.irfanullah <irfansyed.pg@gmail.com>
2018-07-19 17:22:14 +0500

    hello thusday again changes

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // this is irfan syed changesss
+
+        // another cahnges by irfan syed
     }
 }
